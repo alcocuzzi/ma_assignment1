36c4abe612f1700ffcaa62fd7a7eb2db88b8178f
alcocuzzi <alcocuzzi@gmail.com>
2026-02-13 14:57:05 +0000

    TICKET01: Initial Spring Boot project setup with PostgreSQL configuration

---
+++ src/main/java/com/registrytracker/RegistryTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.registrytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryTrackerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryTrackerApplication.class, args);
+    }
+}
